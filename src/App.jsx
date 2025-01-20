6ee7bc7f963e3d13375c16b64683cccb756e2ec3
Nathan Gross <nathan.gross@gmail.com>
2025-01-20 17:20:55 -0600

    Built the getUserBaskets service and implemented it on the main page.
    Co-authored-by: Alex Stout <astout55555@users.noreply.github.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './App.css';
 import ReactDOM from 'react-dom/client';
 import Basket from './components/Basket';
 import Home from './components/Home';
+import basketService from './services';
 
 import {
 	Routes,
@@ -15,17 +16,23 @@ import {
 
 function App() {
 
-  const [ dummyBaskets, setDummyBaskets ] = useState(["abc", "def"]);
+  const [ dummyBaskets, setDummyBaskets ] = useState([]);
 
   // useEffect to grab all baskets so we can map through collection and display
-
+  useEffect(() => {
+    basketService.getUserBaskets()
+      .then((response) => {
+        setDummyBaskets(response);
+      })
+      .catch((error) => console.error(error));
+  }, []);
   //
 
   const allBaskets = (dummyFooBaskets) => {
     console.log(dummyFooBaskets);
-    return dummyFooBaskets.map(basketName => (
-      <li key={basketName}>
-        <Link to={`/basket/view/${basketName}`}>basket {basketName}</Link>
+    return dummyFooBaskets.map(({basket_address}) => (
+      <li key={basket_address}>
+        <Link to={`/basket/view/${basket_address}`}>basket {basket_address}</Link>
       </li>
     ));
   }
@@ -39,7 +46,7 @@ function App() {
       </ul>
 
       <Routes>
-        <Route path="/basket/view/:basketName" element={<Basket />} />
+        <Route path="/basket/view/:basket_address" element={<Basket />} />
         <Route path="/" element={<Home />} />
       </Routes>
     </>
--- src/services.js
+++ src/services.js
@@ -1,13 +1,19 @@
 import axios from 'axios';
 
-const BASE_URL = '/';
+// const BASE_URL = 'https://tamerjohn.work';
+const BASE_URL = 'http://localhost:3000';
+
+async function getUserBaskets() {
+  const response = await axios.get(`${BASE_URL}/all`);
+  return response.data;
+}
 
 async function getBasketContents(basket_address) {
-  const response = await axios.get(`${BASE_URL}/${basket_address}/web`); // may not be correct API endpoint
+  const response = await axios.get(`${BASE_URL}/${basket_address}/web`);
   return response.data;
 }
 
 export default {
   getBasketContents,
-
-}
+  getUserBaskets,
+};
